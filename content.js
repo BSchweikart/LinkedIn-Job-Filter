6a1edce48c75b2c74f0b9921c22fe19706c0a52f
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2025-03-28 03:58:28 -0400

    Create content.js
    Will add the actual code after more testing is completed

---
+++ content.js
@@ -0,0 +1,11 @@
+console.log("Content script is running!");
+
+// Function to hide promoted feed items
+
+// Function to process feed items
+
+// Run feed processing on page load
+
+// Function to hide promoted job listings
+
+// Run job hiding after the entire page has loaded
