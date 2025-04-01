648cd9ffffeb6a23876856c0af974ee046e1a8f8
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2025-03-31 21:22:16 -0400

    Update popup.js

--- Google Job Filter Extension/popup.js
+++ Google Job Filter Extension/popup.js
@@ -1,1 +1,15 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+    const extensionToggle = document.getElementById('extensionToggle');
+  
+    chrome.storage.local.get('extensionEnabled', (data) => {
+      const enabled = data.extensionEnabled === true;
+      extensionToggle.checked = enabled;
+    });
+  
+    extensionToggle.addEventListener('change', () => {
+      const newState = extensionToggle.checked;
+      chrome.storage.local.set({ 'extensionEnabled': newState }, () => {
+        // No need to update button text anymore
+      });
+    });
+  });
