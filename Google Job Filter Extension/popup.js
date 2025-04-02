f0ae42c15b7e13b4c5b48bc7f0e1e5c5593df0ce
Brian Schweikart <31232164+BSchweikart@users.noreply.github.com>
2025-04-01 21:50:32 -0400

    updated version to for popup change and settings page added
    updated version to for popup change and settings page added

--- Google Job Filter Extension/popup.js
+++ Google Job Filter Extension/popup.js
@@ -1,15 +1,33 @@
 document.addEventListener('DOMContentLoaded', () => {
     const extensionToggle = document.getElementById('extensionToggle');
-  
+    const delayUserHiddenCheckboxPopup = document.getElementById('delayUserHiddenPopup');
+    const openOptionsPageLink = document.getElementById('openOptionsPage');
+
+    // Load the saved state of the main extension toggle
     chrome.storage.local.get('extensionEnabled', (data) => {
-      const enabled = data.extensionEnabled === true;
-      extensionToggle.checked = enabled;
+        extensionToggle.checked = data.extensionEnabled === true;
     });
-  
-    extensionToggle.addEventListener('change', () => {
-      const newState = extensionToggle.checked;
-      chrome.storage.local.set({ 'extensionEnabled': newState }, () => {
-        // No need to update button text anymore
-      });
+
+    // Save the state of the main extension toggle when it changes
+    extensionToggle.addEventListener('change', (event) => {
+        chrome.storage.local.set({ 'extensionEnabled': event.target.checked });
     });
-  });
+
+    // Load the saved state for the delay setting and set the popup checkbox
+    chrome.storage.local.get('delayUserHiddenEnabled', (data) => {
+        delayUserHiddenCheckboxPopup.checked = data.delayUserHiddenEnabled === true;
+    });
+
+    // Save the state for the delay setting when the popup checkbox changes
+    delayUserHiddenCheckboxPopup.addEventListener('change', (event) => {
+        chrome.storage.local.set({ 'delayUserHiddenEnabled': event.target.checked });
+    });
+
+    // Add event listener to open the options page
+    if (openOptionsPageLink) {
+        openOptionsPageLink.addEventListener('click', (event) => {
+            event.preventDefault(); // Prevent the link from navigating
+            chrome.runtime.openOptionsPage();
+        });
+    }
+});
